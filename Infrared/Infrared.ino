4b943cc249fa4a627a484f84565b991d0bec4d1d
Thanyasiri <63050146@kmitl.ac.th>
2022-10-21 00:48:47 +0700

    modify Infraled

--- Infrared/Infrared.ino
+++ Infrared/Infrared.ino
@@ -1,27 +1,4 @@
-const int e18Pin =3;
-int ledPin = 13;
-int statusVal = 0;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(e18Pin,INPUT);
-  pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  statusVal = digitalRead(e18Pin);
-  if(statusVal == LOW){
-    //ไม่มีสิ่งกีดขวาง
-    Serial.println("No Collision Detected. ");
-    digitalWrite(ledPin, LOW);
-  }else{
-    //มีสิ่งกีดขวาง
-    Serial.println("Collision Detected. ");
-    digitalWrite(ledPin, HIGH);
-  }
-  delay(100);
-}
+//Infrared photoelectric sensor
 
 /* สงสัยว่าตัวอย่างน่าจะผิด เพราะจากโค้ดแสดงผล Collision Detected. ตอนไม่มีอะไรขวาง แล้วแสดงค่า No Collision Detected. ตอนมีสิ่งกีดขวาง
 const int e18Pin =3;
@@ -42,3 +19,33 @@ void loop() {
   }
 }
 */
+
+//LED
+int ledPin = 13;
+//Infrared
+const int e18Pin =3;
+int statusVal = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  //Infrared
+  pinMode(e18Pin,INPUT);
+  //LED
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  statusVal = digitalRead(e18Pin);
+  if(statusVal == LOW){
+    //ไม่มีสิ่งกีดขวาง ให้ไฟดับ
+    Serial.println("No Collision Detected. ");
+    digitalWrite(ledPin, LOW);
+  }else{
+    //มีสิ่งกีดขวาง ให้ไฟสว่าง
+    Serial.println("Collision Detected. ");
+    digitalWrite(ledPin, HIGH);
+  }
+  delay(100);
+}
