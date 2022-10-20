a43b31eab8f337187269f7f00f5e466fcc63f0f8
Thanyasiri <63050146@kmitl.ac.th>
2022-10-20 23:03:33 +0700

    modify LDR Sensor

--- LDR/LDR.ino
+++ LDR/LDR.ino
@@ -1,12 +1,25 @@
+//LDR Sensor 
+//วัดค่าระดับความเข้มของแสง เป็นค่า 0-1023
+/*
+ให้แบ่งระดับค่าความสว่างเป็น 4 ระดับซึ่งแต่ละช่วงระดับ จะมีหลอด LED ไว้สำหรับการแสดงผล โดยมีข้อกำหนดดังนี้
+ ระดับที่ 1 ให้ LED1 ติด / LED2, LED3, LED4 ดับ
+ ระดับที่ 2 ให้ LED1, LED2 ติด / LED3, LED4 ดับ
+ ระดับที่ 3 ให้ LED1, LED2, LED3 ติด / LED4 ดับ
+ ระดับที่ 4 ให้ LED1, LED2, LED3, LED4 ติด 
+*/
+//หลอดไฟ
 int ledPin1 = 3;
 int ledPin2 = 4;
 int ledPin3 = 5;
 int ledPin4 = 6;
-int con = 1023/4;
+//ค่าสูงสุดที่วัดได้คือ 1023 แบ่งเป็น 4 ระดับ จึงให้ con = 1023/4
+float con = 1023/4;
+//LDR Sensor
 int analogPin = 5;
 int val = 0;
 void setup() {
   // put your setup code here, to run once:
+  //หลอดไฟ
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
@@ -16,11 +29,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //อ่านค่าความเข้มของแสงจาก LDR Sensor ยิ่งสว่งมากค่ายิ่งมาก
   val = analogRead(analogPin);
   Serial.print("LDR = ");
   Serial.println(val);
 
   if(val < con ){
+    //ระดับที่ 1 ค่า 0-255 ให้ LED1 ติด / LED2, LED3, LED4 ดับ
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, LOW);
     digitalWrite(ledPin3, LOW);
@@ -29,6 +44,7 @@ void loop() {
     Serial.println();
   }
   else if(val < con*2){
+    //ระดับที่ 2 ค่า 256-511 ให้ LED1, LED2 ติด / LED3, LED4 ดับ
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, LOW);
@@ -37,6 +53,7 @@ void loop() {
     Serial.println();
   }
   else if(val < con*3){
+    //ระดับที่ 3 ค่า 512-767 ให้ LED1, LED2, LED3 ติด / LED4 ดับ
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, HIGH);
@@ -45,6 +62,7 @@ void loop() {
     Serial.println();
   }
   else{
+    //ระดับที่ 4 ค่า 768-1023 ให้ LED1, LED2, LED3, LED4 ติด 
     digitalWrite(ledPin1, HIGH);
     digitalWrite(ledPin2, HIGH);
     digitalWrite(ledPin3, HIGH);
